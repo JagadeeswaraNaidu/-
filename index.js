24f9302c922489543a5e410d4e9d0e026323a300
Jagadeeswara Naidu <lalamjagadeeswaranaidu08@gmail.com>
2026-03-10 21:30:05 +0530

    trigger CI

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express"
 const app = express()
+// trigger CI/CD pipeline by pushing code to GitHub repository to which this code is connected. This will deploy the code to Heroku and you can see the changes by visiting the URL provided by Heroku after deployment.
 
 app.get("/", (req,res)=>{
 res.send("CI/CD example")
